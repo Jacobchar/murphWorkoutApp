75130ec06b9007626610a974fc317d5b3af9d9dc
Jacob Charlebois <jcharlebois@profireenergy.com>
2022-10-10 20:21:42 -0600

    Project set up; Ready to start coding

--- App.tsx
+++ App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {type PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -78,7 +68,7 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step Three">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
