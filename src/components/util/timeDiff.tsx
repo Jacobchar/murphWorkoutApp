c92521ac21aec8b08cd2fd7a77e6003d368083a5
Jacob Charlebois <jcharlebois@profireenergy.com>
2022-10-20 13:41:35 -0600

    null catch

--- src/components/util/timeDiff.tsx
+++ src/components/util/timeDiff.tsx
@@ -1,24 +1,27 @@
 const timeDiff = (totalTime: string, runStartTime: string) => {
   // Split the time by the separator
-  var totalTimeSplit: string[] = totalTime.split(':');
-  var runStartTimeSplit: string[] = runStartTime.split(':');
+  if (totalTime != null && runStartTime != null) {
+    var totalTimeSplit: string[] = totalTime.split(':');
+    var runStartTimeSplit: string[] = runStartTime.split(':');
 
-  // Compare and subtract each value (assuming that total time is always the greater of the two)
-  var totalTimeInSec: number =
-    Number(totalTimeSplit[0]) * 60 * 60 +
-    Number(totalTimeSplit[1]) * 60 +
-    Number(totalTimeSplit[2]);
-  var runStartTimeInSec: number =
-    Number(runStartTimeSplit[0]) * 60 * 60 +
-    Number(runStartTimeSplit[1]) * 60 +
-    Number(runStartTimeSplit[2]);
+    // Compare and subtract each value (assuming that total time is always the greater of the two)
+    var totalTimeInSec: number =
+      Number(totalTimeSplit[0]) * 60 * 60 +
+      Number(totalTimeSplit[1]) * 60 +
+      Number(totalTimeSplit[2]);
+    var runStartTimeInSec: number =
+      Number(runStartTimeSplit[0]) * 60 * 60 +
+      Number(runStartTimeSplit[1]) * 60 +
+      Number(runStartTimeSplit[2]);
 
-  var timeDiffInSec: number = totalTimeInSec - runStartTimeInSec;
+    var timeDiffInSec: number = totalTimeInSec - runStartTimeInSec;
 
-  var hours: string = minTwoDigits(Math.floor((timeDiffInSec % 3600) / 3600));
-  var mins: string = minTwoDigits(Math.floor((timeDiffInSec % 3600) / 60));
-  var seconds: string = minTwoDigits(Math.floor(timeDiffInSec % 60));
-  return `${hours}:${mins}:${seconds}`;
+    var hours: string = minTwoDigits(Math.floor((timeDiffInSec % 3600) / 3600));
+    var mins: string = minTwoDigits(Math.floor((timeDiffInSec % 3600) / 60));
+    var seconds: string = minTwoDigits(Math.floor(timeDiffInSec % 60));
+    return `${hours}:${mins}:${seconds}`;
+  }
+  return '0';
 };
 
 const minTwoDigits = (num: number) => {
